fe7d47efed58b2ddb0d2bbcbc92880acf2d7a679
Alan <mashalonzo741@gmail.com>
2019-05-03 16:10:58 +0300

    Add add styles and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#design-showing").slideToggle();
+    $("#design-hidden").slideToggle();
+  });
+});
+$(document).ready(function() {
+  $(".click").click(function() {
+    $("#dev-showing").slideToggle();
+    $("#dev-hidden").slideToggle();
+  });
+});
