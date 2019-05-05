2d7c6150b500fc4ddde263fc478ec5c8b995561a
Alan <mashalonzo741@gmail.com>
2019-05-05 19:19:04 +0300

    add function

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $("#design-hidden").slideToggle();
   });
 });
+
 $(document).ready(function() {
   $(".click").click(function() {
     $("#dev-showing").slideToggle();
@@ -15,4 +16,31 @@ $(document).ready(function() {
     $("#pro-showing").slideToggle();
     $("#pro-hidden").slideToggle();
   });
-}); 
+});
+(function () {
+  $(document).ready(function () {
+    return $('#contact-form').submit(function (asc) {
+      var email, message, name;
+      name = document.getElementById('inputName');
+      email = document.getElementById('inputEmail');
+      message = document.getElementById('inputMessage');
+      if (!name.value || !email.value || !message.value) {
+        alertify.error('Please check your entries');
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/mashalonzo741@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json' });
+
+        asc.preventDefault();
+        $(this).get(0).reset();
+        return alertify.success('Message sent');
+      }
+    });
+  });
+
+}).call(this);
+
+//# sourceURL=coffeescript
